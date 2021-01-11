3cea19ff599f048bcae6626909e21b44f901b42f
kentjosol152000 <77250732+kentjosol152000@users.noreply.github.com>
2021-01-11 12:40:43 +0800

    Add files via upload

---
+++ clientserver/clientserver/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace clientserver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.startbutton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.serverporttextBox = new System.Windows.Forms.TextBox();
+            this.serverIPtextBox = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.connectbutton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.clientporttextBox = new System.Windows.Forms.TextBox();
+            this.clientIPtextBox = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.MessagetextBox = new System.Windows.Forms.TextBox();
+            this.sendbutton = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.startbutton);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.serverporttextBox);
+            this.groupBox1.Controls.Add(this.serverIPtextBox);
+            this.groupBox1.Location = new System.Drawing.Point(39, 46);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(494, 82);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Server";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // startbutton
+            // 
+            this.startbutton.Location = new System.Drawing.Point(413, 37);
+            this.startbutton.Name = "startbutton";
+            this.startbutton.Size = new System.Drawing.Size(75, 23);
+            this.startbutton.TabIndex = 6;
+            this.startbutton.Text = "Start";
+            this.startbutton.UseVisualStyleBackColor = true;
+            this.startbutton.Click += new System.EventHandler(this.startbutton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(228, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Port";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "IP";
+            // 
+            // serverporttextBox
+            // 
+            this.serverporttextBox.Location = new System.Drawing.Point(260, 37);
+            this.serverporttextBox.Name = "serverporttextBox";
+            this.serverporttextBox.Size = new System.Drawing.Size(147, 20);
+            this.serverporttextBox.TabIndex = 2;
+            // 
+            // serverIPtextBox
+            // 
+            this.serverIPtextBox.Location = new System.Drawing.Point(43, 37);
+            this.serverIPtextBox.Name = "serverIPtextBox";
+            this.serverIPtextBox.Size = new System.Drawing.Size(139, 20);
+            this.serverIPtextBox.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.connectbutton);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.clientporttextBox);
+            this.groupBox2.Controls.Add(this.clientIPtextBox);
+            this.groupBox2.Location = new System.Drawing.Point(39, 175);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(494, 82);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Client";
+            // 
+            // connectbutton
+            // 
+            this.connectbutton.Location = new System.Drawing.Point(413, 43);
+            this.connectbutton.Name = "connectbutton";
+            this.connectbutton.Size = new System.Drawing.Size(75, 23);
+            this.connectbutton.TabIndex = 9;
+            this.connectbutton.Text = "connect";
+            this.connectbutton.UseVisualStyleBackColor = true;
+            this.connectbutton.Click += new System.EventHandler(this.connectbutton_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(228, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "IP";
+            // 
+            // clientporttextBox
+            // 
+            this.clientporttextBox.Location = new System.Drawing.Point(260, 43);
+            this.clientporttextBox.Name = "clientporttextBox";
+            this.clientporttextBox.Size = new System.Drawing.Size(147, 20);
+            this.clientporttextBox.TabIndex = 6;
+            // 
+            // clientIPtextBox
+            // 
+            this.clientIPtextBox.Location = new System.Drawing.Point(43, 43);
+            this.clientIPtextBox.Name = "clientIPtextBox";
+            this.clientIPtextBox.Size = new System.Drawing.Size(139, 20);
+            this.clientIPtextBox.TabIndex = 4;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(55, 48);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(472, 290);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.Visible = false;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // MessagetextBox
+            // 
+            this.MessagetextBox.Location = new System.Drawing.Point(62, 344);
+            this.MessagetextBox.Multiline = true;
+            this.MessagetextBox.Name = "MessagetextBox";
+            this.MessagetextBox.Size = new System.Drawing.Size(364, 30);
+            this.MessagetextBox.TabIndex = 3;
+            this.MessagetextBox.Visible = false;
+            // 
+            // sendbutton
+            // 
+            this.sendbutton.Location = new System.Drawing.Point(452, 344);
+            this.sendbutton.Name = "sendbutton";
+            this.sendbutton.Size = new System.Drawing.Size(75, 23);
+            this.sendbutton.TabIndex = 10;
+            this.sendbutton.Text = "Send";
+            this.sendbutton.UseVisualStyleBackColor = true;
+            this.sendbutton.UseWaitCursor = true;
+            this.sendbutton.Visible = false;
+            this.sendbutton.Click += new System.EventHandler(this.sendbutton_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 398);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.sendbutton);
+            this.Controls.Add(this.MessagetextBox);
+            this.Controls.Add(this.groupBox1);
+            this.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.Name = "Form1";
+            this.Text = "Inquiry";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button startbutton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox serverporttextBox;
+        private System.Windows.Forms.TextBox serverIPtextBox;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button connectbutton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox clientporttextBox;
+        private System.Windows.Forms.TextBox clientIPtextBox;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox MessagetextBox;
+        private System.Windows.Forms.Button sendbutton;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+    }
+}
+
